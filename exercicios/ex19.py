983e4c6030e5a4e9c4736741751534f40a7530b9
João Victor Leoni <joaovictorleoni2021@gmail.com>
2022-12-07 18:29:56 -0300

    Create ex19.py

---
+++ exercicios/ex19.py
@@ -0,0 +1,5 @@
+nome = input('Informe o seu nome completo:')
+print('Seu nome em maiusculas é {}'.format(nome.upper()))
+print('Seu nome em minusculas é {}'.format(nome.lower()))
+print('Seu nome ao todo tem {}'.format(len(nome)-nome.count(' ')))
+print('Seu primeiro nome tem {} letras'.format(nome.find(' ')))
